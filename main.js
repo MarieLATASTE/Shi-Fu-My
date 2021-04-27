8924657db7cbfa844a9ab85f7a9cb848f69325d4
marieLataste <serialmother4@gmail.com>
2021-04-27 22:16:26 +0200

    translate
    french to english

--- main.js
+++ main.js
@@ -2,31 +2,31 @@ const buttons = document.querySelectorAll('button');
 
 for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener("click", function(){
-        const joueur = buttons[i].innerHTML;
+        const player = buttons[i].innerHTML;
         const robot = buttons[Math.floor(Math.random() * buttons.length)].innerHTML;
         let resultat = "";
         
         //  Logique du jeu
-        if(joueur === robot){
+        if(player === robot){
             resultat = "Egalité";
-        }else if((joueur === "Pierre" && robot === "Ciseaux") ||
-                  (joueur === "Feuille" && robot === "Pierre") || 
-                  (joueur === "Ciseaux" && robot === "Feuille")){
+        }else if((player === "Pierre" && robot === "Ciseaux") ||
+                  (player === "Feuille" && robot === "Pierre") || 
+                  (player === "Ciseaux" && robot === "Feuille")){
             resultat = "Gagné !";
-        }else if((joueur === "Puits" && robot === "Ciseaux") ||
-                   (joueur === "Puits" && robot === "Pierre")){
+        }else if((player === "Puits" && robot === "Ciseaux") ||
+                   (player === "Puits" && robot === "Pierre")){
             resultat = "Gagné avec la technique secrète!";
         }else{
             resultat = "Perdu";
          }
 
          document.querySelector('.resultat').innerHTML = `
-            Joueur: ${joueur} </br>
+            Player: ${player} </br>
             Robot: ${robot} </br>
             ${resultat} !
          `;
 
-        // console.log(`Joueur: ${joueur} vs Robot: ${robot}`);
+        // console.log(`Player: ${player} vs Robot: ${robot}`);
 
     });
 }
